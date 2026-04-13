85628fa5c66bd320767daa05108e36db82e10292
ramoncodedev <ramonwebdesigner2025@gmail.com>
2026-04-13 19:53:19 -0300

    Add application entry point and test configuration

---
+++ src/main/java/com/sales/flow/SalesFlow/SalesFlowApplication.java
@@ -0,0 +1,13 @@
+package com.sales.flow.SalesFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesFlowApplication.class, args);
+	}
+
+}
